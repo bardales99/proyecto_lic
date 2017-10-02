0f8ead2b70fc19355c878f54388f8731f97eac0f
José Bardales <30607275+bardales99@users.noreply.github.com>
2017-10-01 22:37:14 -0600

    Create cerrar.js

---
+++ js/cerrar.js
@@ -0,0 +1,14 @@
+if(window.addEventListener){
+ 	window.addEventListener("load", iniciar, false);
+}
+else if(window.attachEvent){
+ 	window.attachEvent("onload", iniciar);
+}
+function iniciar(){
+	var boton= document.getElementById('cerrar_sesion');
+	if(boton.addEventListener){
+		boton.addEventListener("click",function(){
+			window.location= '../Index.html';
+		},false);
+}
+}
